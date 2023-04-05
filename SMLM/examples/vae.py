f2ff428eb34f13fdc46ad365c82d47efc65f435a
cwseitz <cwseitz2@gmail.com>
2023-04-04 21:52:37 -0400

    update

---
+++ SMLM/examples/vae.py
@@ -0,0 +1,42 @@
+from SMLM.torch import ConvVAE, loss
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import cv2
+
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(64), transforms.CenterCrop(64), transforms.Normalize((0.5,), (0.5,))])
+dataset = datasets.MNIST("data", download=True, transform=transform)
+loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+
+def show(x):
+    img = x.data.cpu().permute(1, 2, 0).numpy() * 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow(img)
+
+#training loop
+beta = 2
+vae = ConvVAE(3)
+opt = torch.optim.Adam(vae.parameters(), lr=5e-4)
+
+for epoch in range(100):
+    print(f'Training epoch {epoch}...')
+    for i, x in enumerate(loader):
+        if len(x) == 2:
+            x = x[0]
+
+        mu, logvar, out = vae(x)
+        rl, kl, l = loss(x, out, mu, logvar, beta)
+        print(f'Loss = {l}')
+        opt.zero_grad()
+        l.backward()
+        #opt.step()
+
+#        if i == 0:
+#            vae.eval()
+#            data = vae.generate(8)
+#            grid_img = torchvision.utils.make_grid(data, nrow=8, normalize=True)
+#            show(grid_img)
+#            vae.train()
