e343d68cce813b306dfadf421848c3fa5ec1abd7
cwseitz <cwseitz2@gmail.com>
2023-04-20 23:52:50 -0400

    update

--- SMLM/examples/dirichlet.py
+++ SMLM/examples/dirichlet.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import uuid
 import matplotlib.pyplot as plt
 from SMLM.psf import *
 from SMLM.localize import *
@@ -10,7 +11,7 @@ from scipy.stats import multivariate_normal
 # Parameters
 #########################
 
-L = 50
+L = 20
 omat = np.ones((L,L))
 gain0 = 2.2 #huang 2013
 offset0 = 0.0
@@ -26,10 +27,10 @@ eta = 0.8 #quantum efficiency
 N0 = 1000
 B0 = 20
 
-nspots = 50
+nspots = 3
 theta0 = np.zeros((5,nspots))
-theta0[0,:] = np.random.uniform(0,L,nspots)
-theta0[1,:] = np.random.uniform(0,L,nspots)
+theta0[0,:] = np.random.normal(L/2,2.0,size=nspots)
+theta0[1,:] = np.random.normal(L/2,2.0,size=nspots)
 theta0[2,:] = sigma
 theta0[3,:] = N0
 theta0[4,:] = B0
@@ -40,19 +41,28 @@ theta0[4,:] = B0
 
 frame = Frame(theta0,eta,texp,L,gain,offset,var)
 adu = frame.generate(plot=True)
-detector = LOGDetector(adu,threshold=50.0)
-spots = detector.detect()
-xvec = spots['x'].to_numpy()
-yvec = spots['y'].to_numpy() 
-n0r = 1000
-niter = 1000
 
+#detector = LOGDetector(adu,threshold=50.0)
+#spots = detector.detect()
+#detector.show(); plt.show()
+xprior = L/2; yprior = L/2
+
+n0r = 1000
+niter = 10
+nchains = 1
+beta = 1e-3
+
+np.savez('/home/cwseitz/Desktop/chains/theta0.npz',theta0)
 dp = DeconDP(adu,eta,texp,gain,var)
-prior_params = (xvec,yvec,sigma,n0r,B0)
+prior_params = (xprior,yprior,sigma,n0r,B0)
 start = time.time()
-thetat = dp.run_mcmc(prior_params,niter,show=False)
+for n in range(nchains):
+    thetat = dp.run_mcmc(prior_params,niter,beta=beta,show=False)
+    chain_id = str(uuid.uuid4())
+    thetat = thetat.assign(chain_id=chain_id)
+    thetat.to_csv('/home/cwseitz/Desktop/chains/' + chain_id + '.csv')
 end = time.time()
 runtime = end - start
-print(f'MCMC Runtime: {runtime}')
+print(f'MCMC Runtime: {runtime} sec')
 print(thetat)
 
--- SMLM/localize/dirichlet.py
+++ SMLM/localize/dirichlet.py
@@ -20,20 +20,20 @@ class DeconDP:
         return np.array(weights[:-1])
 
     def prior(self,prior_params,show=False):
-        xvec,yvec,sigma,n0r,b0 = prior_params
-        nbumps = len(xvec)
+        xprior,yprior,sigma,n0r,b0 = prior_params
         theta = []
-        for n in range(nbumps):
-            weights = self.dirichlet_process(1)
-            mu = np.array([xvec[n],yvec[n]])
-            cov = 0.1*np.eye(2)
-            nspots = len(weights)
-            norm = multivariate_normal(mu,cov)
-            n0 = np.random.poisson(lam=n0r) #prior on signal photons
-            for m in range(nspots):    
-                x0,y0 = norm.rvs() #prior on coordinates
-                weight = weights[m]
-                theta.append([x0,y0,sigma,n0*weight,b0])
+        weights = self.dirichlet_process(1)
+        print(weights)
+        mu = np.array([xprior,yprior])
+        cov = 5*np.eye(2)
+        nspots = len(weights)
+        norm = multivariate_normal(mu,cov)
+        n0 = np.random.poisson(lam=n0r) #prior on signal photons
+        for m in range(nspots):    
+           x0,y0 = norm.rvs() #prior on coordinates
+           weight = weights[m]
+           theta.append([x0,y0,sigma,n0*weight,b0])
+            
         theta = np.array(theta).T
         if show:
             self.show(theta,prior_params)
@@ -47,19 +47,24 @@ class DeconDP:
         ax.scatter(yvec,xvec,color='blue',marker='x')
         plt.show()
         
-    def run_mcmc(self,prior_params,niter,show=False): 
+    def run_mcmc(self,prior_params,niter,beta=5e-5,show=False): 
         data = self.adu
         theta_old = self.prior(prior_params,show=show) #sample from prior       
         like_old = mixloglike(theta_old,data,*self.cmos_params)
         acc = []
         for n in range(niter):
             theta_new = self.prior(prior_params,show=show)
-            theta_new, like, accept = self.accrej(theta_old,theta_new,like_old,data,show=show)
-            theta_new_ = pd.DataFrame({'x0':theta_new[0,:],'y0':theta_new[1,:],'sigma':theta_new[2,:],
-                                       'n0':theta_new[3,:],'b0':theta_new[4,:]})
+            theta_new, like, accept = self.accrej(theta_old,theta_new,like_old,data,beta=beta,show=show)
+            like_old = like
+            theta_new_ = pd.DataFrame({'x0':theta_new[0,:],
+                                       'y0':theta_new[1,:],
+                                       'sigma':theta_new[2,:],
+                                       'n0':theta_new[3,:],
+                                       'b0':theta_new[4,:]})
             theta_new_ = theta_new_.assign(iteration=n,K=theta_new.shape[1])
             self.thetat = pd.concat([self.thetat,theta_new_])
             acc.append(accept)
+        self.diagnostic(acc)
         return self.thetat
 
         
@@ -71,7 +76,7 @@ class DeconDP:
         print(f'Accepted {accept}\n')
 
 
-    def accrej(self,theta_old,theta_new,like_old,data,beta=1e-4,show=False):
+    def accrej(self,theta_old,theta_new,like_old,data,beta=5e-5,show=False):
         accept = True
         like_new = mixloglike(theta_new,data,*self.cmos_params)
         a = np.exp(beta*(like_old-like_new))        
@@ -83,11 +88,17 @@ class DeconDP:
             accept = False
             theta = theta_old
             like = like_old
-            
         self.summarize(like_new,like_old,a,accept)
         return theta, like, accept
         
-        
-
+    def diagnostic(self,acc):
+        acc = np.array(acc)
+        acc = acc.astype(np.int)
+        f = np.cumsum(acc)
+        f = f/np.arange(1,len(f)+1,1)
+        fig, ax = plt.subplots()
+        ax.plot(f,color='black')
+        plt.tight_layout()
+        plt.show()
 
 
