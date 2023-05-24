a0315170b12f727ed15f045a8272df48c688ab61
cwseitz <=>
2023-05-24 02:37:18 +0000

    update

--- SMLM/psf2d/gauss/jaciso.py
+++ SMLM/psf2d/gauss/jaciso.py
@@ -7,32 +7,24 @@ from .ill_auto import *
 
 def jaciso(theta,adu,cmos_params):
     lx, ly = adu.shape
-    ntheta, nspots = theta.shape
+    ntheta = len(theta)
     x0,y0,sigma,N0 = theta
     X,Y = np.meshgrid(np.arange(0,lx),np.arange(0,ly))
     J1 = jacobian1(X,Y,x0,y0,sigma,N0,*cmos_params)
-    J1 = J1.reshape((ntheta*nspots,lx**2))
+    J1 = J1.reshape((ntheta,lx**2))
     J2 = jacobian2(adu,X,Y,x0,y0,sigma,N0,*cmos_params)
     J = J1 @ J2
     return J
-"""
+    
+
 def jaciso_auto(theta,adu,cmos_params):
     eta,texp,gain,var = cmos_params
-    ntheta, nspots = theta.shape
-    theta = theta.reshape((ntheta*nspots,))
+    ntheta = len(theta)
+    theta = theta.reshape((ntheta,))
     ill = isologlike_auto(adu,eta,texp,gain,var)
     jacobian_ = jacobian(ill)
     jac = jacobian_(theta)
     return jac
 
  
-def jaciso(theta,adu,eta,texp,gain,var):
-    lx, ly = adu.shape
-    ntheta = len(theta)
-    x0,y0,sigma,N0 = theta
-    X,Y = np.meshgrid(np.arange(0,lx),np.arange(0,ly))
-    J1 = jacobian1(X,Y,x0,y0,sigma,N0,eta,texp,gain,var)
-    J2 = jacobian2(adu,X,Y,x0,y0,sigma,N0,eta,texp,gain,var)
-    J = np.sum(J1*J2[np.newaxis,:],axis=(1,2))
-    return J
-"""
+
--- SMLM/torch/pred/optimize.py
+++ SMLM/torch/pred/optimize.py
@@ -22,3 +22,29 @@ class Optimizer:
             theta[1] -= eta*jac[1]
         self.plot(self.theta0,theta)
         return theta
+        
+class SGLDOptimizer:
+    def __init__(self,theta0,adu,cmos_params):
+        self.theta0 = theta0
+        self.adu = adu
+        self.cmos_params = cmos_params
+    def plot(self,theta0,theta):
+        fig, ax = plt.subplots()
+        ax.imshow(self.adu,cmap='gray')
+        ax.scatter([theta0[0]],[theta0[1]],marker='x',color='red')
+        ax.scatter([theta[0]],[theta[1]],marker='x',color='blue')
+        plt.show()
+    def optimize(self,iters=1000,eta=0.001):
+        ntheta = len(self.theta0)
+        theta = np.zeros((iters,ntheta))
+        theta[0,:] = self.theta0
+        for n in range(1,iters):
+            jac = jaciso(theta[n-1,:],self.adu,self.cmos_params)
+            eps1 = np.random.normal(0,1)
+            eps2 = np.random.normal(0,1)
+            theta[n,0] = theta[n-1,0] - eta*jac[0] + np.sqrt(eta)*eps1
+            theta[n,1] = theta[n-1,1] - eta*jac[1] + np.sqrt(eta)*eps2
+            theta[n,2] = theta[n-1,2]
+            theta[n,3] = theta[n-1,3]
+        #self.plot(self.theta0,theta)
+        return theta
