27366ed7ee195e889a13bf1403d5e84c553e575e
cwseitz <=>
2023-06-13 17:47:46 +0000

    update

--- SMLM/generators/ts3d.py
+++ SMLM/generators/ts3d.py
@@ -4,6 +4,7 @@ import os
 import secrets
 import string
 import json
+import scipy.sparse as sp
 
 from skimage.io import imsave
 from scipy.special import erf
@@ -12,6 +13,7 @@ from scipy.special import factorial
 
 from SSA._SSA import photoswitch
 from .bin_ssa import bin_ssa
+from ..utils import *
 from ..psf.psf3d.psf3d import *
 from perlin_noise import PerlinNoise
 
@@ -35,19 +37,20 @@ class TimeSeries3D:
         self.alpha = config['alpha']
         self.beta = config['beta']
         self.zmin = config['zmin']
-        self.pixel_size = config['pixel_size']
+        self.pixel_size_lateral = config['pixel_size_lateral']
         self.kvec = np.array([config['k12'],config['k23'],config['k34'],config['k21'],config['k31'],config['k41']]) 
         self.kvec = 1e-3*self.kvec      
-        self.zhrange = 2000.0
+        self.zhrange = config['zhrange']
+        self.pixel_size_axial = 2*self.zhrange/self.nz
         self.theta = np.zeros((5,self.particles))
         self.theta[0,:] = np.random.uniform(10,self.nx-10,self.particles)
         self.theta[1,:] = np.random.uniform(10,self.ny-10,self.particles)
-        self.theta[2,:] = np.random.uniform(-self.zhrange,self.zhrange,self.particles)
+        self.theta[2,:] = np.random.uniform(-self.zhrange/self.pixel_size_axial,self.zhrange/self.pixel_size_axial,self.particles)
         self.theta[3,:] = self.sigma
         self.theta[4,:] = self.N0
         self.nparams,self.nparticles = self.theta.shape
         self.r = int(self.texp/self.dt)
-        
+
     def ssa(self):
         k12,k23,k34,k21,k31,k41 = self.kvec
         nt = int(round(self.T/self.dt))
@@ -76,7 +79,7 @@ class TimeSeries3D:
         self.rnoise_adu = self.rnoise_adu.astype(np.int16) #round
         
         self.adu = self.signal_adu + self.backrd_adu + self.rnoise_adu
-        self.get_spikes()
+        self.boolean_grid()
 
     def get_brate(self):
         nt = int(round(self.T/self.texp))
@@ -94,37 +97,39 @@ class TimeSeries3D:
         x = np.arange(0,2*patch_hw); y = np.arange(0,2*patch_hw)
         X,Y = np.meshgrid(x,y)
         self.srate = np.zeros((nt,self.nx,self.ny),dtype=np.float32)
-        gtmat = []
+        self.xyz_np = np.zeros((nt,self.nparticles,3+1))
         for t in range(nt):
             print(f'Signal frame {t}')
             for n in range(self.nparticles):
                 x0,y0,z0,sigma,N0 = self.theta[:,n]
                 patchx, patchy = int(round(x0))-patch_hw, int(round(y0))-patch_hw
                 x0p = x0-patchx; y0p = y0-patchy
-                sigma_x = sx(sigma,z0,self.zmin,self.alpha)
-                sigma_y = sy(sigma,z0,self.zmin,self.beta)
+                sigma_x = sx(sigma,z0*self.pixel_size_axial,self.zmin,self.alpha)
+                sigma_y = sy(sigma,z0*self.pixel_size_axial,self.zmin,self.beta)
                 lam = lamx(X,x0p,sigma_x)*lamy(Y,y0p,sigma_y)
                 fon = self.state[n,0,t*self.r:self.r*(t+1)]
                 fon = np.sum(fon)/len(fon)
                 mu = fon*self.texp*self.eta*N0*lam
                 self.srate[t,patchx:patchx+2*patch_hw,patchy:patchy+2*patch_hw] += mu
-                if fon > 0:
-                    gtmat.append([t,fon,x0,y0,z0])     
-        self.gtmat = np.array(gtmat)
+                self.xyz_np[t,n] = [x0,y0,z0,fon]
         
-    def get_spikes(self,upsample=4):
+    def boolean_grid(self,upsample=4):
         nt = int(round(self.T/self.texp))
-        self.spikes = []
-        for n in range(nt):
-           rows = self.gtmat[self.gtmat[:,0] == n]
-           pos = rows[:,2:5]
-           rr = [[0,self.nx],[0,self.nx],[-self.zhrange,self.zhrange]]
-           bins = [upsample*self.nx,upsample*self.nx,self.nz]
-           for point in pos:
-               idx = tuple(int(np.floor((p - r[0]) / (r[1] - r[0]) * b)) for p, r, b in zip(point, rr, bins))
-               self.spikes.append(idx)
-        self.spikes = np.array(self.spikes)
-   
+        grid_shape = (self.nx,self.ny,self.nz)
+        boolean_grid_sparse = batch_xyz_to_boolean_grid(self.xyz_np,upsample,
+                                                        self.pixel_size_lateral,
+                                                        self.pixel_size_axial,
+                                                        self.zhrange,
+                                                        grid_shape)
+        self.spikes = boolean_grid_sparse
+        indices = boolean_grid_sparse.coalesce().indices()
+        values = boolean_grid_sparse.coalesce().values()
+        size = boolean_grid_sparse.size()
+
+        # Create a scipy sparse matrix
+        print(values)
+        sparse_matrix = sp.csc_matrix((values, indices), shape=size)
+        
     def shot_noise(self,rate):
         nt,nx,ny = rate.shape
         electrons = np.zeros_like(rate)
@@ -152,7 +157,7 @@ class TimeSeries3D:
         imsave(datapath+fname+'-rnoise_adu.tif',self.rnoise_adu,imagej=True)
         with open(datapath+fname+'.json', 'w') as f:
             json.dump(self.config, f)
-        np.savez(datapath+fname+'_mask.npz',spikes=self.spikes)
+        np.savez(datapath+fname+'_spikes.npz',spikes=self.spikes)
         np.savez(datapath+fname+'_ssa.npz',state=self.state)
-        np.savez(datapath+fname+'_gtmat.npz',gtmat=self.gtmat)
+        np.savez(datapath+fname+'_xyz_np.npz',xyz_np=self.xyz_np)
 
---
+++ SMLM/utils/data_utils.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+
+# converts continuous xyz locations to a boolean grid
+def batch_xyz_to_boolean_grid(xyz_np, upsampling_factor, pixel_size_lateral, pixel_size_axial, zhrange, grid_shape):
+       
+    # current dimensions
+    H, W, D = grid_shape
+        
+    # number of particles
+    batch_size, num_particles, nfeatures = xyz_np.shape
+    pixel_size_lateral /= upsampling_factor
+    zshift = xyz_np[:, :, 2] + zhrange/pixel_size_axial
+        
+    xg = (np.floor(xyz_np[:, :, 0]/pixel_size_lateral)).astype('int')
+    yg = (np.floor(xyz_np[:, :, 1]/pixel_size_lateral)).astype('int')
+    zg = (np.floor(zshift/pixel_size_axial)).astype('int')
+    
+    # indices for sparse tensor
+    indX, indY, indZ = (xg.flatten('F')).tolist(), (yg.flatten('F')).tolist(), (zg.flatten('F')).tolist()
+
+    # update dimensions
+    H, W = int(H * upsampling_factor), int(W * upsampling_factor)
+    
+    # if sampling a batch add a sample index
+    if batch_size > 1:
+        indS = (np.kron(np.ones(num_particles), np.arange(0, batch_size, 1)).astype('int')).tolist()
+        ibool = torch.LongTensor([indS, indZ, indY, indX])
+    else:
+        ibool = torch.LongTensor([indZ, indY, indX])
+
+    # spikes for sparse tensor
+    vals = torch.ones(batch_size*num_particles)
+    
+    # resulting 3D boolean tensor
+    if batch_size > 1:
+        boolean_grid = torch.sparse.FloatTensor(ibool, vals, torch.Size([batch_size, D, H, W]))
+    else:
+        boolean_grid = torch.sparse.FloatTensor(ibool, vals, torch.Size([D, H, W]))
+        
+    return boolean_grid
+
